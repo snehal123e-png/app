a0bf00294d58d04bc64ff025bf01acdf79b01e1e
snehal123e-png <snehal123e@gmail.com>
2026-07-25 22:22:20 +0530

    mongoosee

--- chat-app/backend/routes/UserRoutes.js
+++ chat-app/backend/routes/UserRoutes.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const router = express.Router();
-const User = require("../models/User");
 const { readDB } = require("../db");
 
 router.get("/", async (req, res) => {
