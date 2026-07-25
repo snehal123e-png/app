a42fe94a69fec5e8e087820adb1932ff6d261057
snehal123e-png <snehal123e@gmail.com>
2026-07-25 22:32:58 +0530

    done ok

--- chat-app/backend/server.js
+++ chat-app/backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 // --- Routes ---
 app.use('/api/auth', authRoutes);
 app.use('/api/chat', chatRoutes);
-app.use('/api/users', userRoutes);
+app.use('/api/users', UserRoutes);
 
 app.get('/', (req, res) => {
   res.send('Chat API is running!');
