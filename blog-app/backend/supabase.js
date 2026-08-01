cff4f809f0f762f603cfc92411b53f6ca1e97724
snehal123e-png <snehal123e@gmail.com>
2026-08-01 13:51:34 +0530

    hayeee

--- blog-app/backend/supabase.js
+++ blog-app/backend/supabase.js
@@ -1,11 +1,11 @@
 const { createClient } = require("@supabase/supabase-js");
 
 console.log("SUPABASE_URL =", process.env.SUPABASE_URL);
-console.log("KEY EXISTS =", !!process.env.SUPABASE_PUBLISHABLE_KEY);
+console.log("KEY EXISTS =", !!process.env.SUPABASE_SERVICE_ROLE_KEY);
 
 const supabase = createClient(
     process.env.SUPABASE_URL,
-    process.env.SUPABASE_PUBLISHABLE_KEY
+    process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 module.exports = supabase;
